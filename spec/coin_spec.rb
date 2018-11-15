e18aacf8fec4505c03f95e624f87dce4f52ffad7
Ben Kirby and Paige Williams <paige@>
2018-11-15 10:29:54 -0800

    Its finalized  will spit out a sentence that correctly identifies the coins for a given value

--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -4,23 +4,19 @@ require ('coin')
 describe (".Cents") do
   it("Takes a number less than five and determines the number of cents")do
     cents = Cents.new(1)
-    expect(cents.coins).to(eq([1,0,0,0]))
+    expect(cents.coins).to(eq("You have 1 pennies, 0 nickels, 0 dimes, and 0 quarters."))
   end
 
   it("Takes a number less than five and determines the number of cents")do
     cents = Cents.new(5)
-    expect(cents.coins).to(eq([0,1,0, 0]))
+    expect(cents.coins).to(eq("You have 0 pennies, 1 nickels, 0 dimes, and 0 quarters."))
   end
 
   it("Takes a number greater than five and determines the number of cents")do
     cents = Cents.new(87)
-    expect(cents.coins).to(eq([2,0,1,3]))
+    expect(cents.coins).to(eq("You have 2 pennies, 0 nickels, 1 dimes, and 3 quarters."))
   end
 
-  it("Takes a number greater than five and determines the number of cents")do
-    cents = Cents.new(104)
-    expect(cents.coins).to(eq([4,0,0,4]))
-  end
   it("It returns the number of cents in plain english")do
     cents = Cents.new(104)
     expect(cents.coins).to(eq("You have 4 pennies, 0 nickels, 0 dimes, and 4 quarters."))
