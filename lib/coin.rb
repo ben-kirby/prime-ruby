46dd763efa9cdfd7377147cc5d3b3c4b999802d7
Ben Kirby and Paige Williams <paige@>
2018-11-15 10:24:43 -0800

    It will now have a readable response for the number of coins return to a given amount.

--- lib/coin.rb
+++ lib/coin.rb
@@ -5,19 +5,36 @@ class Cents
     @amount = amount
   end
 
-  def find_pennies
-    pennies = @amount
-    return "There are #{pennies}."
-  end
+  def coins
+    @pennies = 0
+    @quarters = 0
+    @dimes = 0
+    @nickels = 0
 
-  def find_nickels
-    nickels = 0
-    until (@amount < 5)
-      @amount -= 5
-      nickels += 1
+    if (@amount >= 25)
+      until (@amount < 25)
+        @amount -= 25
+        @quarters += 1
+      end
     end
-    nickels
+
+    if (@amount < 25 && @amount >= 10)
+      until (@amount < 10)
+        @amount -= 10
+        @dimes += 1
+      end
+    end
+
+    if (@amount < 10 && @amount >= 5)
+      until (@amount < 5)
+        @amount -= 5
+        @nickels += 1
+      end
+    end
+
+    if (@amount <5)
+      @pennies = @amount
+    end
+    return "You have #{@pennies} pennies, #{@nickels} nickels, #{@dimes} dimes, and #{@quarters} quarters."
   end
-
-
 end
--- spec/coin_spec.rb
+++ spec/coin_spec.rb
@@ -4,12 +4,26 @@ require ('coin')
 describe (".Cents") do
   it("Takes a number less than five and determines the number of cents")do
     cents = Cents.new(1)
-    expect(cents.find_pennies()).to(eq("There are 1."))
+    expect(cents.coins).to(eq([1,0,0,0]))
   end
 
   it("Takes a number less than five and determines the number of cents")do
-    cents = Cents.new(11)
-    expect(cents.find_nickels()).to(eq(2))
+    cents = Cents.new(5)
+    expect(cents.coins).to(eq([0,1,0, 0]))
+  end
+
+  it("Takes a number greater than five and determines the number of cents")do
+    cents = Cents.new(87)
+    expect(cents.coins).to(eq([2,0,1,3]))
+  end
+
+  it("Takes a number greater than five and determines the number of cents")do
+    cents = Cents.new(104)
+    expect(cents.coins).to(eq([4,0,0,4]))
+  end
+  it("It returns the number of cents in plain english")do
+    cents = Cents.new(104)
+    expect(cents.coins).to(eq("You have 4 pennies, 0 nickels, 0 dimes, and 4 quarters."))
   end
 
 end
